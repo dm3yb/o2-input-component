d17b0247aec83008e483ba4793b8947f6049f5a9
Dmitry Bobryshev <dmitry.b09@gmail.com>
2025-02-16 15:51:39 +0100

    style: add input & layout & section styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import { BaseInput } from './components/ui/BaseInput';
 
 export default function App() {
 	return (
-		<main className='input-wrapper'>
-			<h1>O2 Input Component</h1>
-			<form action='#' method='POST' name='username'>
-				<BaseInput />
-			</form>
+		<main className='section-one'>
+			<div className='container'>
+				<h1>O2 Input Component</h1>
+				<form action='#' method='POST' name='username'>
+					<BaseInput
+						label='Input'
+						type='text'
+						name='username'
+						ariaLabel='username'
+						placeholder='Placeholder'
+						isOptional
+					/>
+				</form>
+			</div>
 		</main>
 	);
 }
--- src/components/ui/BaseInput.tsx
+++ src/components/ui/BaseInput.tsx
@@ -1,3 +1,40 @@
-export const BaseInput: React.FC = () => {
-	return <input type='text' placeholder='Placeholder' aria-label='username' />;
+type Props = {
+	label: string;
+	name: string;
+	type: React.HTMLInputTypeAttribute | undefined;
+	placeholder: string;
+	ariaLabel: string;
+	isOptional?: boolean;
+};
+
+export const BaseInput: React.FC<Props> = ({
+	label,
+	type,
+	name,
+	placeholder,
+	ariaLabel,
+	isOptional = false,
+}) => {
+	return (
+		<div className='base-input-wrapper'>
+			<div className='base-input-label-wrapper'>
+				<label htmlFor={name} className='base-input-label'>
+					{label}
+				</label>
+				{isOptional && (
+					<label htmlFor={name} className='base-input-label optional-label'>
+						Optional
+					</label>
+				)}
+			</div>
+			<input
+				id={name}
+				name={name}
+				type={type}
+				placeholder={placeholder}
+				aria-label={ariaLabel}
+				className='base-input'
+			/>
+		</div>
+	);
 };
